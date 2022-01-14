106dee0c88face8892c0406037ca875b2ff63b14
rsshonjoydas <rsshonjoydas@gmail.com>
2022-01-14 17:33:57 +0600

    SideBar

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import SideBar from '../components/SideBar';
+
+const Home = () => (
+  <div>
+    <SideBar />
+  </div>
+);
+
+export default Home;
